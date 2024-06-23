9f88a281d0e0646ad959e8312db832440e142df7
diegoperea20 <diegoperea20@gmail.com>
2024-06-23 17:05:51 -0500

    added link

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ const Home = () => {
       <QrScanner />
       <div className="project-github">
       <p>This project is in </p>
-      <Link href="https://github.com/diegoperea20">
+      <Link href="https://github.com/diegoperea20/Qr-Scanner">
         <img width="96" height="96" src="https://img.icons8.com/fluency/96/github.png" alt="github"/>
       </Link>
     </div>
