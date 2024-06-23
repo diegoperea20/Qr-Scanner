319d8d11e02eab55ee4d146fbec0bd8c679f2253
diegoperea20 <diegoperea20@gmail.com>
2024-06-23 17:46:42 -0500

    change mode

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import QrScanner from '../components/Qrscanner.jsx';
+import Qrscanner from '@/components/Qrscanner';
 import Link from 'next/link';
 
 const Home = () => {
   return (
     <div>
       <h1>QR Code Scanner</h1>
-      <QrScanner />
+      <Qrscanner />
       <div className="project-github">
       <p>This project is in </p>
       <Link href="https://github.com/diegoperea20/Qr-Scanner">
